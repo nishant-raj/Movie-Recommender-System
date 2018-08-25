8c0d95c567f7f18dd02815ab54f1ee59cf109157
nishant-raj <42694755+nishant-raj@users.noreply.github.com>
2018-08-25 17:18:47 +0530

    Add files via upload

---
+++ content based filtering.py
@@ -0,0 +1,40 @@
+#recommended the movies based on the similarity of their content like tagline and overview
+#I used Tf-Idf for 1 & 2 ngrams words to finds similarity
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+
+md = pd. read_csv('E:/data/movielens/movies_metadata.csv')
+links_small = pd.read_csv('E:/data/movielens/links_small.csv')
+links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
+
+md = md.drop([19730, 29503, 35587])
+md['id'] = md['id'].astype('int')
+smd = md[md['id'].isin(links_small)]
+smd.shape
+smd['tagline'] = smd['tagline'].fillna('')
+smd['description'] = smd['overview'] + smd['tagline']
+smd['description'] = smd['description'].fillna('')
+
+#Text-frequency Inverse-Document-Frequency
+tf = TfidfVectorizer(analyzer='word',ngram_range=(1,2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(smd['description'])
+
+#similarity between two words in a document
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+cosine_sim[0]
+smd = smd.reset_index()
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+	
+get_recommendations('The Godfather').head(10)	
